2711011d966be97f89d7cc84308e1447785cdad6
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-03-03 13:34:05 +0100

    fix: Removed WeatherForecast, test commit

--- YoutubeLinks.Api/Program.cs
+++ YoutubeLinks.Api/Program.cs
@@ -21,30 +21,6 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithTags("WeatherForecast");
-
 app.UseCORS();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
