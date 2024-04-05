0e865b7811272a2e85fd4ebcc400a4b39a50e28a
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-04-05 20:50:59 +0200

    feat(Email): Create application email (for sending emails from app) #12

---
+++ YoutubeLinks.Api/Data/Migrations/20240405183007_AppEmail.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YoutubeLinks.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AppEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created", "Email", "Modified" },
+                values: new object[] { new DateTime(2024, 4, 5, 18, 30, 7, 34, DateTimeKind.Utc).AddTicks(2726), "ytlinksapp@gmail.com", new DateTime(2024, 4, 5, 18, 30, 7, 34, DateTimeKind.Utc).AddTicks(2726) });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Created", "Email", "Modified" },
+                values: new object[] { new DateTime(2024, 4, 5, 18, 30, 7, 34, DateTimeKind.Utc).AddTicks(2726), "ytlinksapp1@gmail.com", new DateTime(2024, 4, 5, 18, 30, 7, 34, DateTimeKind.Utc).AddTicks(2726) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created", "Email", "Modified" },
+                values: new object[] { new DateTime(2024, 4, 4, 16, 42, 44, 179, DateTimeKind.Utc).AddTicks(7086), "freakfightsfan@gmail.com", new DateTime(2024, 4, 4, 16, 42, 44, 179, DateTimeKind.Utc).AddTicks(7086) });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Created", "Email", "Modified" },
+                values: new object[] { new DateTime(2024, 4, 4, 16, 42, 44, 179, DateTimeKind.Utc).AddTicks(7086), "freakfightsfan1@gmail.com", new DateTime(2024, 4, 4, 16, 42, 44, 179, DateTimeKind.Utc).AddTicks(7086) });
+        }
+    }
+}
