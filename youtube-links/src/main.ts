afd38778187847ad77b08240677d71e655c8b0a2
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-04-08 12:10:04 +0200

    feat(Vue): vuetify button test

--- youtube-links/src/main.ts
+++ youtube-links/src/main.ts
@@ -6,9 +6,21 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
---
+++ youtube-links/src/views/TestView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="about">
+        <h1>This is test page</h1>
+        <v-btn variant="tonal" class="ml-2" @click="handleClick">
+            vuetify button test
+        </v-btn>
+    </div>
+</template>
+
+<script setup lang="ts">
+const handleClick = () => {
+    console.log("BUTTON CLICKED");
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
