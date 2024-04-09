ce7cafc5dedeb81dd9cca9eef4bc0988028534eb
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-04-09 14:43:26 +0200

    feat(Unit Tests): GetAllPublicPlaylistsFeatureTests

---
+++ YoutubeLinks.UnitTests/Features/Playlists/Queries/GetAllPublicPlaylistsFeatureTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using MediatR;
+using NSubstitute;
+using YoutubeLinks.Api.Data.Entities;
+using YoutubeLinks.Api.Data.Repositories;
+using YoutubeLinks.Api.Features.Playlists.Queries;
+using YoutubeLinks.Shared.Abstractions;
+using YoutubeLinks.Shared.Features.Playlists.Queries;
+using YoutubeLinks.Shared.Features.Playlists.Responses;
+
+namespace YoutubeLinks.UnitTests.Features.Playlists.Queries
+{
+    public class GetAllPublicPlaylistsFeatureTests
+    {
+        [Fact]
+        public async Task GetAllPublicPlaylistsHandler_ReturnsPlaylistsPagedList()
+        {
+            var query = new GetAllPublicPlaylists.Query
+            {
+                Page = 1,
+                PageSize = 10,
+                SortColumn = "name",
+                SortOrder = SortOrder.Ascending,
+                SearchTerm = "",
+            };
+
+            var list = new List<Playlist>()
+            {
+                new()
+                {
+                    Id = 1,
+                }
+            };
+
+            var playlistRepository = Substitute.For<IPlaylistRepository>();
+            var mediator = Substitute.For<IMediator>();
+
+            playlistRepository.AsQueryablePublic().Returns(list.AsQueryable());
+
+            mediator.Send(Arg.Any<GetAllPublicPlaylists.Query>(), CancellationToken.None)
+                .Returns(callInfo =>
+                {
+                    var handler = new GetAllPublicPlaylistsFeature.Handler(playlistRepository);
+                    return handler.Handle(callInfo.Arg<GetAllPublicPlaylists.Query>(), CancellationToken.None);
+                });
+
+            var result = await mediator.Send(query, CancellationToken.None);
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType<PagedList<PlaylistDto>>();
+            result.TotalCount.Should().Be(1);
+            result.Items.Count.Should().Be(1);
+        }
+    }
+}
