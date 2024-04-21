7472f16ff1aeaf4eada41482edabc3cf09f059c4
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-04-22 01:29:54 +0200

    feat(Unit Tests): CreateLinkTests

---
+++ YoutubeLinks.UnitTests/Features/Links/Commands/CreateLinkTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation.TestHelper;
+using YoutubeLinks.Shared;
+using YoutubeLinks.Shared.Features.Links.Commands;
+using YoutubeLinks.Shared.Localization;
+using YoutubeLinks.UnitTests.Localization;
+
+namespace YoutubeLinks.UnitTests.Features.Links.Commands
+{
+    public class CreateLinkTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("    ")]
+        [InlineData("   ")]
+        public void CreateLinkValidator_Url_ShouldNotBeEmpty(string url)
+        {
+            var message = "Youtube video url should not be empty.";
+
+            var localizer = new TestStringLocalizer<ValidationMessage>();
+            localizer.AddTranslation(nameof(ValidationMessageString.UrlNotEmpty), message);
+
+            var validator = new CreateLink.Validator(localizer);
+
+            var command = new CreateLink.Command
+            {
+                Url = url,
+                PlaylistId = 1
+            };
+
+            var result = validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Url)
+                  .WithErrorMessage(message);
+        }
+
+        [Theory]
+        [InlineData("asd")]
+        [InlineData("www.google.com")]
+        [InlineData("test.test.com")]
+        [InlineData("https://www.youtube.com/watch?v=")]
+        [InlineData("https://www.youtube.com/watch?v=asd")]
+        [InlineData("https://www.youtube.com/watch")]
+        public void CreateLinkValidator_Url_ShouldMatchYoutubeVideoRegex(string url)
+        {
+            var message = "This is not a valid link to the YouTube video.";
+
+            var localizer = new TestStringLocalizer<ValidationMessage>();
+            localizer.AddTranslation(nameof(ValidationMessageString.VideoUrlMatchesRegex), message);
+
+            var validator = new CreateLink.Validator(localizer);
+
+            var command = new CreateLink.Command
+            {
+                Url = url,
+                PlaylistId = 1
+            };
+
+            var result = validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Url)
+                  .WithErrorMessage(message);
+        }
+    }
+}
---
+++ YoutubeLinks.UnitTests/Localization/TestStringLocalizer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Globalization;
+
+namespace YoutubeLinks.UnitTests.Localization
+{
+    public class TestStringLocalizer<T> : IStringLocalizer<T>
+    {
+        private readonly Dictionary<string, string> _localizations;
+
+        public TestStringLocalizer()
+        {
+            _localizations = new Dictionary<string, string>();
+        }
+
+        public LocalizedString this[string name]
+        {
+            get
+            {
+                return new LocalizedString(name, _localizations.ContainsKey(name) ? _localizations[name] : name, true);
+            }
+        }
+
+        public LocalizedString this[string name, params object[] arguments]
+        {
+            get
+            {
+                var format = _localizations.ContainsKey(name) ? _localizations[name] : name;
+                var formatted = string.Format(CultureInfo.CurrentCulture, format, arguments);
+                return new LocalizedString(name, formatted, true);
+            }
+        }
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTranslation(string key, string value)
+        {
+            _localizations[key] = value;
+        }
+    }
+}
