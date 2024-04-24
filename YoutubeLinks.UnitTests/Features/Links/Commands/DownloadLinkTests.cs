4234e56fe4e9c4dedb191b72df4c0de9ec2bbf7b
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-04-24 15:02:02 +0200

    feat(Unit Tests): DownloadLinkTests

---
+++ YoutubeLinks.UnitTests/Features/Links/Commands/DownloadLinkTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation.TestHelper;
+using YoutubeLinks.Shared;
+using YoutubeLinks.Shared.Features.Links.Commands;
+using YoutubeLinks.Shared.Features.Links.Helpers;
+using YoutubeLinks.Shared.Localization;
+using YoutubeLinks.UnitTests.Localization;
+
+namespace YoutubeLinks.UnitTests.Features.Links.Commands
+{
+    public class DownloadLinkTests
+    {
+        [Theory]
+        [InlineData((YoutubeFileType)2)]
+        [InlineData((YoutubeFileType)3)]
+        [InlineData((YoutubeFileType)4)]
+        [InlineData((YoutubeFileType)5)]
+        public void DownloadLinkValidator_YoutubeFileType_ShouldBeInEnum(YoutubeFileType youtubeFileType)
+        {
+            var message = string.Format("YoutubeFileType has a range of values which does not include: {0}.", youtubeFileType);
+
+            var localizer = new TestStringLocalizer<ValidationMessage>();
+            localizer.AddTranslation(nameof(ValidationMessageString.YoutubeFileTypeIsInEnum), message);
+
+            var validator = new DownloadLink.Validator(localizer);
+
+            var command = new DownloadLink.Command
+            {
+                Id = 1,
+                YoutubeFileType = youtubeFileType,
+            };
+
+            var result = validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.YoutubeFileType)
+                  .WithErrorMessage(message);
+        }
+    }
+}
