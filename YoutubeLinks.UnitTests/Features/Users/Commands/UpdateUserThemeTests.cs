379ac53172d261d09eabad122e8ac44eed7b7be6
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-05-06 00:41:48 +0200

    feat(Unit Tests): UpdateUserThemeTests

---
+++ YoutubeLinks.UnitTests/Features/Users/Commands/UpdateUserThemeTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.TestHelper;
+using YoutubeLinks.Shared;
+using YoutubeLinks.Shared.Features.Users.Commands;
+using YoutubeLinks.Shared.Features.Users.Helpers;
+using YoutubeLinks.Shared.Localization;
+using YoutubeLinks.UnitTests.Localization;
+
+namespace YoutubeLinks.UnitTests.Features.Users.Commands
+{
+    public class UpdateUserThemeTests
+    {
+        [Theory]
+        [InlineData((ThemeColor)3)]
+        [InlineData((ThemeColor)4)]
+        [InlineData((ThemeColor)5)]
+        public void UpdateUserThemeValidator_YoutubeFileType_ShouldBeInEnum(ThemeColor themeColor)
+        {
+            var message = string.Format("ThemeColor has a range of values which does not include: {0}.", themeColor);
+
+            var localizer = new TestStringLocalizer<ValidationMessage>();
+            localizer.AddTranslation(nameof(ValidationMessageString.ThemeColorIsInEnum), message);
+
+            var validator = new UpdateUserTheme.Validator(localizer);
+
+            var command = new UpdateUserTheme.Command
+            {
+                Id = 1,
+                ThemeColor = themeColor,
+            };
+
+            var result = validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.ThemeColor)
+                  .WithErrorMessage(message);
+        }
+    }
+}
