1856f0a19edc5dd403d121811db9ff7de1e23e02
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-05-29 18:51:22 +0200

    feat(E2E): DownloadLinkAsMP4 test

--- YoutubeLinks.E2E/LinksPageTests.cs
+++ YoutubeLinks.E2E/LinksPageTests.cs
@@ -91,7 +91,24 @@ namespace YoutubeLinks.E2E
         [Test]
         public async Task DownloadLinkAsMP4()
         {
+            var title = "Rick Astley - Never Gonna Give You Up (Official Music Video)";
+            var url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            await CreateLink(title, url);
 
+            var downloadTask = Page.WaitForDownloadAsync(new PageWaitForDownloadOptions { Timeout = 60000 });
+            await ApiResponseOkAfterButtonClick(LinksPageConst.DownloadMP4FileButton, "links");
+            var download = await downloadTask;
+
+            var testProjectDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..");
+            var tempFolderPath = Path.Combine(testProjectDirectoryPath, "Temp");
+            var savePath = Path.Combine(tempFolderPath, download.SuggestedFilename);
+
+            if (!Directory.Exists(tempFolderPath))
+                Directory.CreateDirectory(tempFolderPath);
+
+            await download.SaveAsAsync(savePath);
+
+            await DeleteLink(title);
         }
 
         [Test]
