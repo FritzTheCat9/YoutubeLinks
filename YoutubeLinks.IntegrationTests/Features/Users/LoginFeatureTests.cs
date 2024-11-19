1074406528efbec69bb421cddc159f06e40df6f3
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-11-19 17:14:55 +0100

    :white_check_mark: Login Integration Tests

--- YoutubeLinks.IntegrationTests/Features/Users/LoginFeatureTests.cs
+++ YoutubeLinks.IntegrationTests/Features/Users/LoginFeatureTests.cs
@@ -1,5 +1,3 @@
-using System.Net;
-using System.Net.Http.Json;
 using FluentAssertions;
 using YoutubeLinks.Shared.Features.Users.Commands;
 
@@ -8,47 +6,33 @@ namespace YoutubeLinks.IntegrationTests.Features.Users;
 public class LoginFeatureTests(IntegrationTestWebAppFactory factory)
     : BaseIntegrationTest(factory)
 {
-    private const string Url = "/api/users/login";
-
     [Fact]
-    public async Task GIVEN_WHEN_THEN()
-    {
-        var command = new Login.Command
-        {
-            Email = "ytlinksapp@gmail.com", Password = "Asd123!"
-        };
-        
-        var response = await Client.PostAsJsonAsync($"{Url}", command);
-
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
-        
-        Context.Users.Count().Should().Be(2);
-    }
-    
-    [Fact]
-    public async Task GIVEN_WHEN_THEN2()
-    {
-        var command = new Login.Command
-        {
-            Email = "ytlinksapp@gmail.com", Password = "Asd123!!"
-        };
-        
-        var response = await Client.PostAsJsonAsync($"{Url}", command);
-
-        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    }
-    
-    [Fact]
-    public async Task Login_ShouldReturnJwtToken()
+    public async Task Login_WithValidCredentials_ShouldLoginAsAdmin()
     {
         var command = new Login.Command
         {
             Email = "ytlinksapp@gmail.com", Password = "Asd123!"
         };
 
-        var result = await UserApiClient.Login(command);
+        var jwt = await UserApiClient.Login(command);
 
-        Assert.NotNull(result);
-        Assert.False(string.IsNullOrEmpty(result.AccessToken));
+        jwt.Should().NotBeNull();
+        jwt.AccessToken.Should().NotBeNull();
+        jwt.RefreshToken.Should().NotBeNull();
+    }
+    
+    [Fact]
+    public async Task Login_WithValidCredentials_ShouldLoginAsUser()
+    {
+        var command = new Login.Command
+        {
+            Email = "ytlinksapp1@gmail.com", Password = "Asd123!"
+        };
+
+        var jwt = await UserApiClient.Login(command);
+
+        jwt.Should().NotBeNull();
+        jwt.AccessToken.Should().NotBeNull();
+        jwt.RefreshToken.Should().NotBeNull();
     }
 }
