25527896ae98cb2375ae2b29b9f9c1ff90019c51
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-11-21 20:44:46 +0100

    ✅ Add integration tests for ExportPlaylist feature in ExportPlaylistFeatureTests

---
+++ YoutubeLinks.IntegrationTests/Features/Playlists/Commands/ExportPlaylistFeatureTests.cs
@@ -0,0 +1,45 @@
+using FluentAssertions;
+using YoutubeLinks.Api.Data.Entities;
+using YoutubeLinks.Shared.Features.Playlists.Commands;
+using YoutubeLinks.Shared.Features.Playlists.Helpers;
+
+namespace YoutubeLinks.IntegrationTests.Features.Playlists.Commands;
+
+public class ExportPlaylistFeatureTests(IntegrationTestWebAppFactory factory)
+    : BaseIntegrationTest(factory)
+{
+    [Fact]
+    public async Task ExportPlaylist_ShouldSucceed_WhenDataIsValid()
+    {
+        var user = await LoginAsAdmin();
+
+        var playlist = new Playlist { Name = "TestPlaylist", Public = true, UserId = user.UserId };
+        await Context.Playlists.AddAsync(playlist);
+        await Context.SaveChangesAsync();
+
+        await Logout();
+
+        var command = new ExportPlaylist.Command
+        {
+            Id = playlist.Id,
+            PlaylistFileType = PlaylistFileType.Json,
+        };
+
+        var response = await PlaylistApiClient.ExportPlaylist(command);
+
+        response.IsSuccessStatusCode.Should().BeTrue();
+        response.Content.Headers.ContentType?.MediaType.Should().Be("application/json");
+        
+        await using (var stream = await response.Content.ReadAsStreamAsync())
+        {
+            using var memoryStream = new MemoryStream();
+            await stream.CopyToAsync(memoryStream);
+            memoryStream.Length.Should().BeGreaterThan(0);
+        }
+
+        var filename = response.Content.Headers.ContentDisposition?.FileNameStar;
+        var playlistNameWithExtension =
+            $"{playlist.Name}.{PlaylistHelpers.PlaylistFileTypeToString(PlaylistFileType.Json)}";
+        filename.Should().Be(playlistNameWithExtension);
+    }
+}
