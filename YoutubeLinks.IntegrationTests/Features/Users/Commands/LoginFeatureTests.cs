74cff0b37aaae3dac7b6f690ea01984a3e7bc910
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-11-25 21:59:33 +0100

    ✅ Add integration tests for Login feature in LoginFeatureTests

--- YoutubeLinks.IntegrationTests/Features/Users/Commands/LoginFeatureTests.cs
+++ YoutubeLinks.IntegrationTests/Features/Users/Commands/LoginFeatureTests.cs
@@ -1,3 +1,8 @@
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using YoutubeLinks.Api.Data.Entities;
+using YoutubeLinks.Shared.Features.Users.Commands;
+
 namespace YoutubeLinks.IntegrationTests.Features.Users.Commands;
 
 public class LoginFeatureTests(IntegrationTestWebAppFactory factory)
@@ -8,10 +13,42 @@ public class LoginFeatureTests(IntegrationTestWebAppFactory factory)
     {
         await LoginAsAdmin();
     }
-    
+
     [Fact]
     public async Task Login_ShouldSucceed_WhenUserCredentialsAreValid()
     {
         await LoginAsUser();
+    }
+
+    [Fact]
+    public async Task Login_ShouldSucceed_WhenDataIsValid()
+    {
+        var user = new User
+        {
+            Email = "testuser@gmail.com",
+            UserName = "TestUser",
+            Password = "AQAAAAIAAYagAAAAECWFTp9uY78qPzaRu0d3uaJNo3WOlRpwCuCyDLH+yg/TowsjzlMGxMurTnvyZaYSxA==",
+            EmailConfirmed = true,
+            IsAdmin = false,
+        };
+
+        await Context.Users.AddAsync(user);
+        await Context.SaveChangesAsync();
+
+        var command = new Login.Command()
+        {
+            Email = user.Email,
+            Password = "Asd123!",
+        };
+
+        var jwt = await UserApiClient.Login(command);
+        jwt.AccessToken.Should().NotBeNull();
+        jwt.RefreshToken.Should().NotBeNull();
+
+        var modifiedUser = await Context.Users.AsNoTracking().FirstOrDefaultAsync(x => x.Id == user.Id);
+
+        modifiedUser.Should().NotBeNull();
+        modifiedUser?.RefreshToken.Should().NotBeNull();
+        modifiedUser?.RefreshToken.Should().Be(jwt.RefreshToken);
     }
 }
